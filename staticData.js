c7a1b6b41a029b51766251004768ebd969880ab9
durgaprasadshalvaltd <durgaprasadshalvaltd@gmail.com>
2024-08-31 11:52:53 +0530

    Add files via upload

---
+++ staticData.js
@@ -0,0 +1,35 @@
+const intros = [
+    {
+        welcomeText: "Hi, I'm",
+        firstName: "Durga prasad",
+        lastName: "Shalva",
+        caption: "Innovator-Developer-Freelancer",
+        description: "I am a developer. Aspiring to create web applications. I am also a Java and C developer with understanding of SQL, React, Bootstrap, Tailwind, and Node. Looking for a respected position in a good firm.",
+    },
+];
+
+const about = [
+    {
+        lottieURL: "https://lottie.host/81c8ae49-bf6d-4aa7-af8a-41c2ed94c6ab/z1r7o6fBob.json",
+        description1:"Hello! My name is Durga Prasad Shalva. I adore developing things that exist on the internet. My passion in web creation began in 2022, when I decided to build a website for my mother's small business. This taught me a lot about HTML and JavaScript! Following graduation from college, I immediately began working on my projects and learning as much as possible. My goal is to develop websites that are intuitive, user-friendly, and functional.",
+        description2:"I enjoy creating user-friendly websites that are accessible to anybody. I also enjoy making my clients satisfied with the performance of their websites, so they can view them on any device they own. I've always been interested in studying new technologies and languages, and it's become a passion of mine. I enjoy that we are continuously pushing the frontiers of what is possible in web design and development, which allows me to work on both tough and rewarding projects. That being said, I love that we're making a meaningful difference in people's lives and the globe as a whole.",
+        skills: [
+            "JavaScript",
+            "React",
+            "Express",
+            "MongoDB",
+            "Node",
+        ],
+    },
+];
+
+const contacts = [
+    {
+        name: "Durga prasad Shalva",
+        gender: "Male",
+        age: "23",
+        email: "shalvadurgaprasad020@gmail.com",
+        mobile: "8341242690",
+        address: "INDIA",
+    }
+]
