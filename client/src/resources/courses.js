5f9b86e867bb2ebed14e21ac15cb254134e878fa
durgaprasadshalvaltd <durgaprasadshalvaltd@gmail.com>
2024-08-31 12:53:26 +0530

    Add files via upload

---
+++ client/src/resources/courses.js
@@ -0,0 +1,32 @@
+export const courses = [
+    {
+        title: "Complete Web Development",
+        description: "I learned the latest technologies, such as JavaScript, React, Node, and even Web3 development.Creating any website I want. Create completely functional websites and web apps for your company or business. Mastered frontend development with React. Learned backend development with Node. I learned about professional development best practices.",
+        image: "https://i.ibb.co/q7Cgvmc/Complete-Front-End.jpg",
+        link: "https://drive.google.com/file/d/1dZLEvF-mzne-QEthcIVjfToYKzxM7V6s/view?usp=sharing",
+    },
+    {
+        title: "Meta Front-End Development",
+        description: "Can build a responsive website utilizing HTML to arrange content, CSS to control visual style, and JavaScript to create interactive experiences. I learned how to use React with JavaScript libraries and frameworks. I learned how to use the Bootstrap CSS Framework to develop webpages and interact with GitHub repositories and version control. Learn the best ways to problem solving and create portfolio-ready projects to present during employment interviews.",
+        image: "https://i.ibb.co/R9gPHzz/Meta-frontend.jpg",
+        link: "https://coursera.org/verify/professional-cert/YLXAWY5TF2D9",
+    },
+    {
+        title: "Cybersecurity Essentials",
+        description: "Understand important phrases and ideas in cyber security.Analyze threats and hazards in the context of the cybersecurity landscape.Assessed cybersecurity decision-making results. Gain a thorough understanding of key cybersecurity principles. Developed the necessary skills for a job in cybersecurity. Took efforts to establish a cybersecurity culture within a company.",
+        image: "https://i.ibb.co/h26X0zQ/Cybercurity-Essentials.jpg",
+        link: "https://drive.google.com/file/d/1HSyJRBYLfRMNiBVBpgNnesk6vrxSdQjw/view?usp=sharing",
+    },
+    {
+        title: "Foundations Data, Data, Everywhere",
+        description: "Define and explain essential ideas in data analytics, such as data, analysis, and data ecosystems.Can do a self-assessment of analytical thinking, providing particular instances of its use.Spreadsheets, query languages, and data visualization tools were discussed in the context of data analytics.Can describe a data analyst's role in terms of distinct jobs.",
+        image: "https://i.ibb.co/zmtg62w/UX.jpg",
+        link: "https://coursera.org/verify/NSRBF32CWZQW",
+    },
+    {
+        title: "Foundations of User Experience (UX) Design, by Google",
+        description: "Identify the common job responsibilities of entry-level UX designers and other teams you may collaborate with. Understand fundamental ideas in UX design, such as user-centered design, the design process, accessibility, and equity-driven design. Can explain why design sprints are an essential and beneficial component of a UX designer's job.",
+        image: "https://i.ibb.co/GMkDFg5/Foundations-Data-Data-Everywhere.jpg",
+        link: "https://coursera.org/verify/ZSDF5LQK23PJ",
+    },
+]
---
+++ client/src/resources/projects.js
@@ -0,0 +1,37 @@
+export const projects = [
+    {
+        technologies:[],
+        title: "Personal blog website",
+        image: "https://i.ibb.co/p1wtMgY/10782771-19197307-Photoroom.png",
+        description: "Website to showcase my portfolio, let others know how to approach me",
+        link: "https://durgaprasadshalva.github.io/Website/",
+    },
+    {
+        technologies:[],
+        title: "Library Management system",
+        image: "https://www.skoolbeep.com/blog/wp-content/uploads/2020/12/HOW-DO-YOU-DESIGN-A-LIBRARY-MANAGEMENT-SYSTEM-min.png",
+        description: "A smart records management application to sort problems of librarian",
+        link: "https://pdfhost.io/v/TSm20.UeZ_Library_Management_system_using_jav",
+    },
+    {
+        technologies:[],
+        title: "Smart home Automation",
+        image: "https://i.ibb.co/QNQL5S3/5596259-55810-Photoroom.png",
+        description: "A complete power saving home automation with human sensing technology",
+        link: "/",
+    },
+    {
+        technologies:[],
+        title: "Wheel driven sprayer",
+        image: "https://i.ibb.co/z6zjZpS/file-1.png",
+        description: "A pesticide sprayer with auto powered pump to save energy and efficency",
+        link: "https://pdfhost.io/v/xGGqYltkO_Retail_Design",
+    },
+    {
+        technologies:[],
+        title: "Automated hydroponic farming system",
+        image: "https://i.ibb.co/YQ0fK3v/greenhouse-isometric-composition.png",
+        description: "fully controlled farming system to get high yield and cost effective",
+        link: "https://drive.google.com/file/d/1jBJM3iU_CEaT2xnhXmLGgoWlBm5BMOqt/view?usp=sharing",
+    },
+];
